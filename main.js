cd7170338f82f0657a0a5a318c4229ba35d5e6a0
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 13:46:53 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+let popped = myFish.pop();
+
+console.log(myFish); // ['angel', 'clown', 'mandarin' ]
+
+console.log(popped); // 'sturgeon'
